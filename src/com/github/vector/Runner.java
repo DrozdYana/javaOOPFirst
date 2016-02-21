c98a62f7268abe07c34d876a7325073fc8121731
Yana <you@example.com>
2016-02-21 19:00:32 +0200

    created class Vector3d

--- src/com/github/vector/Runner.java
+++ src/com/github/vector/Runner.java
@@ -3,13 +3,13 @@ package com.github.vector;
 public class Runner {
 
 	public static void main(String[] args) {
-		Vector vectorOne=new Vector(-1,2,16);
-		Vector vectorTwo=new Vector(2,4,10);
-		Vector vectorThree=vectorOne.addVector(vectorTwo);
+		Vector3d vectorOne=new Vector3d(-1,2,16);
+		Vector3d vectorTwo=new Vector3d(2,4,10);
+		Vector3d vectorThree=vectorOne.addVector(vectorTwo);
 		System.out.println(vectorOne+" + "+vectorTwo+" = "+vectorThree);
 		double scalarMultiply=vectorTwo.scalarMultiply(vectorOne,vectorTwo);
 		System.out.println(vectorOne+" * "+vectorTwo+" = "+scalarMultiply);
-		Vector vectorFour=vectorTwo.multiplyVector(vectorThree);
+		Vector3d vectorFour=vectorTwo.multiplyVector(vectorThree);
 		System.out.println(vectorTwo+" x "+vectorThree+" = "+vectorFour);
 
 	}
--- src/com/github/vector/Vector3d.java
+++ src/com/github/vector/Vector3d.java
@@ -1,11 +1,11 @@
 package com.github.vector;
 
-public class Vector {
+public class Vector3d {
 	private double x;
 	private double y;
 	private double z;
 
-	public Vector(double x, double y, double z) {
+	public Vector3d(double x, double y, double z) {
 		super();
 		this.x = x;
 		this.y = y;
@@ -36,25 +36,25 @@ public class Vector {
 		this.z = z;
 	}
 
-	public Vector addVector(Vector vector) {
+	public Vector3d addVector(Vector3d vector) {
 		double a = this.x + vector.x;
 		double b = this.y + vector.y;
 		double c = this.z + vector.z;
 
-		return new Vector(a, b, c);
+		return new Vector3d(a, b, c);
 	}
 
-	public double scalarMultiply(Vector vectorOne, Vector vectorTwo) {
+	public double scalarMultiply(Vector3d vectorOne, Vector3d vectorTwo) {
 		double multiply = vectorOne.x * vectorTwo.x + vectorOne.y * vectorTwo.y + vectorOne.z * vectorTwo.z;
 
 		return multiply;
 	}
 
-	public Vector multiplyVector(Vector vector) {
+	public Vector3d multiplyVector(Vector3d vector) {
 		double a = this.y * vector.z - this.z * vector.y;
 		double b = this.z * vector.x - this.x * vector.z;
 		double c = this.x * vector.y - this.y * vector.x;
-		return new Vector(a, b, c);
+		return new Vector3d(a, b, c);
 
 	}
 
